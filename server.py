5501cf6ef4ad85f73282d03cfd50e7ac5e74df6e
JoseRodriguez-CR <josemariorodriguezc@gmail.com>
2021-09-24 19:18:31 -0600

    Uploading Files

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, session, redirect
+
+app = Flask(__name__)
+
+app.secret_key = "EsojGnidoc"
+
+@app.route( '/')
+def index():
+    if "counter" not in session:
+        session["counter"] = 1
+    else:
+        session['counter'] += 1
+    return render_template( "index.html" )
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run( debug = True )
